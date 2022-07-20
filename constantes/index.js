785009cc6ae3e383f1b4e14998b244a3bf5946e2
Daphiny Santos <91172155+daphi-ny@users.noreply.github.com>
2022-07-20 13:24:34 -0300

    Add files via upload

---
+++ constantes/index.js
@@ -0,0 +1,31 @@
+/*var valor = 15;
+valor = 20;
+console.log(valor); */
+
+/*var cliente = {
+    nome: 'fulano',
+    idade: 20,
+    email: 'fulano@gmail.com',        Objetos
+    aprovacao: true
+};
+
+console.log(cliente); */
+
+/*
+var itens = [0, 1, 'coisa', 8];      Arrays
+console.log(itens[3]); */
+
+
+/*var cor = 'vermelho';
+function trocaCor () {               
+    cor = '';
+};                                  
+
+console.log(cor);
+trocaCor();
+console.log(cor);
+                                    Funções
+function multipliclar (valor) {
+    return valor * 4;
+}
+console.log(multipliclar(5))  */
