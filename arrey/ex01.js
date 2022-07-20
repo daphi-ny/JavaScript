7996e359707237a6418551c5fef7794c7c166d89
Daphiny Santos <91172155+daphi-ny@users.noreply.github.com>
2022-07-20 13:24:12 -0300

    Add files via upload

---
+++ arrey/ex01.js
@@ -0,0 +1,8 @@
+//var arr = new Array(); //usar o new para cirar um obj
+//var arr = new Array(n1, n2, n3, n4); // a criamos um array já iniciado com elementos
+
+var arr = [1, 2, 3, 4]; // Acessamos os valores do Array pela notação de colchetes
+
+arr [0] = 20;
+arr [1] = 15;
+document.write(arr[1]); // exibe o conteudo
