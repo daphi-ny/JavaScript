afdd518d95419fbc02716fcd344ad7f4e5fa7486
Daphiny Santos <91172155+daphi-ny@users.noreply.github.com>
2022-07-20 13:25:52 -0300

    Add files via upload

---
+++ swich/index.js
@@ -0,0 +1,27 @@
+var mes = 'agosto';
+switch (mes) {
+    case 'janeiro':
+    case 'fevereiro':
+    case 'março':
+        document.write('primavera');
+        break;
+
+    case 'abril':
+    case 'maio':
+    case 'junho':
+        document.write('outono');
+        break;   
+        
+    case 'julho':
+    case 'agosto':
+    case 'setembro':
+        document.write('inverno');
+        break;      
+
+    case 'outrubro':
+    case 'novembro':
+    case 'dezembro':
+        document.write('verão');
+        break;
+
+}
