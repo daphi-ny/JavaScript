05d254ff95c78589daadef17de1114de06f55b6c
Daphiny Santos <91172155+daphi-ny@users.noreply.github.com>
2022-07-20 13:25:06 -0300

    Add files via upload

---
+++ if_elseif_else/index.js
@@ -0,0 +1,15 @@
+//if tradicional
+
+//var n1 = 1;
+//var n2 = 2;
+
+//if (n1 < n2) {          
+  //  window.alert('2 é maior que 1');
+//} else {
+ //   window.alert('1 é maior que 2');
+//}
+
+// if abreviado
+
+var n1 = 8;
+(n1 >= 5 ? window.alert('sim') : window.alert('não'));
